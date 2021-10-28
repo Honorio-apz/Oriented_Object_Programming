14df0e3d5899d7bb10ab48699009fd14379032a1
HONORIO APAZA <honorio.apz@gmail.com>
2021-10-28 16:18:52 -0500

    proyecto con bd

---
+++ empleados/src/Clases/Conectar.java
@@ -0,0 +1,19 @@
+
+package Clases;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+public class Conectar {
+    Connection cn;
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/empleados","Honorio","1234");
+            System.out.println("Conexion existosa");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "No se pudo conectar a base de  datos");
+        }
+    return cn;
+    }  
+}
